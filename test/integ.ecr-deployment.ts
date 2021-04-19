a0ea2755090fa2c2cf0ac29b5948f5c5864c60d2
wchaws <68052927+wchaws@users.noreply.github.com>
2021-04-19 11:48:19 +0800

    chore: update test/integ.ecr-deployment.ts (#9)
    Fixes #

--- test/integ.ecr-deployment.ts
+++ test/integ.ecr-deployment.ts
@@ -1,12 +1,19 @@
+import * as path from 'path';
+import { DockerImageAsset } from '@aws-cdk/aws-ecr-assets';
 import * as cdk from '@aws-cdk/core';
 import * as ecrDeploy from '../src/index';
 
 class TestECRDeployment extends cdk.Stack {
   constructor(scope: cdk.App, id: string) {
     super(scope, id);
-    new ecrDeploy.ECRDeployment(this, 'DeployMe', {
-      src: new ecrDeploy.DockerImageName('jsii/superchain'),
-      dest: new ecrDeploy.DockerImageName('1234.dkr.ecr.us-west-2.amazonaws.com/test:jsii'),
+
+    const image = new DockerImageAsset(this, 'CDKDockerImage', {
+      directory: path.join(__dirname, 'docker'),
+    });
+
+    new ecrDeploy.ECRDeployment(this, 'DeployDockerImage', {
+      src: new ecrDeploy.DockerImageName(image.imageUri),
+      dest: new ecrDeploy.DockerImageName(`${cdk.Aws.ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/test:nginx`),
     });
   }
 }
