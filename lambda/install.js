8724be87f3af6c206344b575a2e972b0175e5444
javacs3 <javacs3+aws@gmail.com>
2021-10-16 18:30:18 +0800

    chore: add install.js

---
+++ lambda/install.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const got = require('got');
+const path = require('path');
+const stream = require('stream');
+const crypto = require('crypto');
+
+const { promisify } = require('util');
+const pipeline = promisify(stream.pipeline);
+
+const package = require('../package.json');
+const version = package.version;
+const rootUrl = package.repository.url;
+
+function mkdirp(p) {
+  if (!fs.existsSync(p)) {
+    fs.mkdirSync(p, { recursive: true });
+  }
+}
+
+function sha256sum(p) {
+  return new Promise(function (resolve, reject) {
+    const hash = crypto.createHash('sha256');
+
+    fs.createReadStream(p)
+      .on('error', reject)
+      .on('data', chunk => hash.update(chunk))
+      .on('close', () => resolve(hash.digest('hex')));
+  });
+}
+
+async function download(url, dest) {
+  console.log(`download ${url}`);
+  await pipeline(
+    got.stream(url),
+    fs.createWriteStream(dest)
+  );
+}
+
+
+
+(async () => {
+  const dir = process.argv[2];
+  if (!dir) {
+    throw new Error('Missing an argument');
+  }
+  mkdirp(dir);
+
+  const integrity = (await got(`${rootUrl}/releases/download/v${version}/main.sha256`)).body;
+  const bin = path.join(dir, 'main');
+  await download(`${rootUrl}/releases/download/v${version}/main`, bin);
+  const checksum = await sha256sum(bin);
+
+  if (checksum !== integrity) {
+    throw new Error('Checksum error');
+  }
+})().catch(err => {
+  console.error(err);
+  process.exit(1);
+})
