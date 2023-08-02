9007bd139d7dea739c1e6f5ff3ea49dec752ec61
Torben Neufeldt <torbenneufeldt@gmail.com>
2023-08-02 11:53:13 +0200

    Use proxy to download main.sha256

--- lambda/install.js
+++ lambda/install.js
@@ -28,10 +28,7 @@ function sha256sum(p) {
   });
 }
 
-async function download(url, dest) {
-  const agent = {};
-  agent.https = process.env.HTTPS_PROXY ? new HttpsProxyAgent({proxy: process.env.HTTPS_PROXY}): undefined;
-  agent.http = process.env.HTTP_PROXY ? new HttpProxyAgent({proxy: process.env.HTTP_PROXY}): undefined;
+async function download(url, dest, agent) {
   console.log(`download ${url}`);
   await pipeline(
     got.stream(url, { agent }),
@@ -51,8 +48,12 @@ async function download(url, dest) {
   const bin = path.join(dir, 'main');
 
   if (!fs.existsSync(bin)) {
-    await download(`${rootUrl}/releases/download/v${version}/main`, bin);
-    const expectedIntegrity = (await got(`${rootUrl}/releases/download/v${version}/main.sha256`)).body.trim();
+    const agent = {};
+    agent.https = process.env.HTTPS_PROXY ? new HttpsProxyAgent({proxy: process.env.HTTPS_PROXY}): undefined;
+    agent.http = process.env.HTTP_PROXY ? new HttpProxyAgent({proxy: process.env.HTTP_PROXY}): undefined;
+
+    await download(`${rootUrl}/releases/download/v${version}/main`, bin, agent);
+    const expectedIntegrity = (await got(`${rootUrl}/releases/download/v${version}/main.sha256`, { agent })).body.trim();
     const integrity = await sha256sum(bin);
 
     if (integrity !== expectedIntegrity) {
