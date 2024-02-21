c8e619223007f2d81b6431043222f75de7e8ea53
wchaws <68052927+wchaws@users.noreply.github.com>
2024-02-21 09:01:43 +0000

    chore: enable lambda test

--- test/lambda.test.ts
+++ test/lambda.test.ts
@@ -5,7 +5,7 @@
 import { spawnSync } from 'child_process';
 import * as path from 'path';
 
-test.skip('lambda python pytest', () => {
+test('lambda python pytest', () => {
   const result = spawnSync(path.join(__dirname, 'lambda', 'test.sh'), { stdio: 'inherit' });
   expect(result.status).toBe(0);
 });
