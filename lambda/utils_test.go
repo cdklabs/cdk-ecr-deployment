14104cf9bbe76bd97a2570897e23634d02cf881e
Billy Bafia <bafiawilliam@gmail.com>
2025-03-11 22:30:52 -0500

    add unit tests for parsing secret json.

--- lambda/utils_test.go
+++ lambda/utils_test.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,4 +31,29 @@ func TestGetCredsType(t *testing.T) {
 	assert.Equal(t, SECRET_NAME, GetCredsType("fake-secret"))
 	assert.Equal(t, SECRET_TEXT, GetCredsType("username:password"))
 	assert.Equal(t, SECRET_NAME, GetCredsType(""))
+}
+
+func TestParseJsonSecret(t *testing.T) {
+	secretJson := "{\"username\":\"user_val\",\"password\":\"pass_val\"}"
+	isValid := json.Valid([]byte(secretJson))
+	assert.True(t, isValid)
+
+	successCase, noError := ParseJsonSecret(secretJson)
+	assert.NoError(t, noError)
+	assert.Equal(t, "user_val:pass_val", successCase)
+
+	failParseCase, jsonParseError := ParseJsonSecret("{\"user}")
+	assert.Equal(t, "", failParseCase)
+	assert.Error(t, jsonParseError)
+	assert.Contains(t, "json unmarshal error: unexpected end of JSON input", jsonParseError.Error())
+
+	noUsernameCase, usernameError := ParseJsonSecret("{\"password\":\"pass_val\"}")
+	assert.Equal(t, "", noUsernameCase)
+	assert.Error(t, usernameError)
+	assert.Contains(t, "json username error", usernameError.Error())
+
+	noPasswordCase, passwordError := ParseJsonSecret("{\"username\":\"user_val\"}")
+	assert.Equal(t, "", noPasswordCase)
+	assert.Error(t, passwordError)
+	assert.Contains(t, "json password error", passwordError.Error())
 }
