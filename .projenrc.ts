b77b99f526dd19e030855f037b8baf1a1940bd4a
Momo Kornher <kornherm@amazon.co.uk>
2025-03-13 14:45:44 +0000

    fix: remove unnecessary bundled dependencies (#1102)
    Removes ~8MB from the published package. These dependencies used to be
    need to download the prebuilt lambda handler from GitHub. Since we don't
    do that anymore, we don't need them anymore!
    
    <img width="778" alt="image"
    src="https://github.com/user-attachments/assets/e7e51a63-78e2-4aa6-bd65-e71cbc3672e0"
    />
    
    Also cleaning up some other minor stuff in `.projenrc.ts`.
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,41 +4,41 @@
 import { CdklabsConstructLibrary } from 'cdklabs-projen-project-types';
 
 const project = new CdklabsConstructLibrary({
-  setNodeEngineVersion: false,
+  name: 'cdk-ecr-deployment',
   stability: 'stable',
   private: false,
-  author: 'wchaws',
+  projenrcTs: true,
+
+  description: 'CDK construct to deploy docker image to Amazon ECR',
+  repositoryUrl: 'https://github.com/cdklabs/cdk-ecr-deployment',
+  author: 'Amazon Web Services',
   authorAddress: 'https://aws.amazon.com',
-  cdkVersion: '2.80.0',
-  cdkVersionPinning: false,
+
   defaultReleaseBranch: 'main',
   majorVersion: 3,
-  enablePRAutoMerge: true,
-  name: 'cdk-ecr-deployment',
-  projenrcTs: true,
   publishToPypi: {
     distName: 'cdk-ecr-deployment',
     module: 'cdk_ecr_deployment',
-  }, /* Publish to pypi. */
-  bundledDeps: [
-    'got',
-    'hpagent',
-  ],
-  deps: [
-    'got',
-    'hpagent',
-  ], /* Runtime dependencies of this module. */
+  },
+
   jsiiVersion: '5.7.x',
   typescriptVersion: '5.7.x',
-  description: 'CDK construct to deploy docker image to Amazon ECR', /* The description is just a string that helps people understand the purpose of the package. */
-  repositoryUrl: 'https://github.com/cdklabs/cdk-ecr-deployment', /* The repository is the location where the actual code for your package lives. */
+  cdkVersion: '2.80.0',
+  cdkVersionPinning: false,
+  bundledDeps: [],
+  deps: [],
+
   gitignore: [
-    'cdk.out/',
+    'cdk.out',
     'lambda-bin/bootstrap',
-  ], /* Additional entries to .gitignore. */
+  ],
   npmignore: [
-    '/cdk.out',
-  ], /* Additional entries to .npmignore. */
+    'cdk.out',
+    'build-lambda.sh',
+  ],
+
+  enablePRAutoMerge: true,
+  setNodeEngineVersion: false,
 });
 
 project.package.addField('jsiiRosetta', {
@@ -48,10 +48,5 @@ project.package.addField('jsiiRosetta', {
 });
 
 project.preCompileTask.exec('./build-lambda.sh');
-
-project.package.addField('resolutions', {
-  'trim-newlines': '3.0.1',
-  'xmldom': 'github:xmldom/xmldom#0.7.0', // TODO: remove this when xmldom^0.7.0 is released in npm
-});
 
 project.synth();
